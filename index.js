ce0a02047df7372a8f2dcedc30aae545837990d9
aron k koech <varokoaruna81@gmail.com>
2024-10-01 13:35:38 +0300

    adds 4 functions

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(start) {
+    return Math.abs(start - 42); 
+  }
+  
+
+function distanceFromHqInFeet(feet) {
+   return (distanceFromHqInBlocks(feet))*264;
+
+}
+
+ function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264;
+  }
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return 2.56;
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far'; 
+    }
+  }
